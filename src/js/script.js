3112e5a7b24243175a2fbc2254687f6851414c94
Taylor Radtke <tradtke@aapa.org>
2018-04-17 12:05:03 -0400

    cme cert dropdown

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,21 @@ function initMap() {
     map,
   });
 }
+
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};

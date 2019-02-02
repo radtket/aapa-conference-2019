d4c681e8d6a983f794f63dc9e5cde07532dc0054
Taylor Radtke <taylorradtke@gmail.com>
2019-02-01 19:14:46 -0500

    added README.md

---
+++ README.md
@@ -0,0 +1,48 @@
+# Landing Page - AAPA 2019 Conference
+
+An early bird splash landing page for the AAPA 2019 Conference
+
+**Live demo:** [https://radtket.github.io/aapa-conference-2019/](https://radtket.github.io/aapa-conference-2019/)
+
+## Getting started
+
+### Clone repo
+
+```bash
+# clone the repo
+$ git clone https://github.com/radtket/aapa-conference-2019.git my-project
+
+# go into app's directory
+$ cd my-project
+
+# install app's dependencies
+$ npm install
+```
+
+## Usage
+
+```bash
+# serve with hot reload at localhost:3000.
+$ npm run start
+
+# build for production with minification
+$ npm run build
+```
+
+## What's included
+
+Within the download you'll find the following directories and files, logically grouping common assets and providing both compiled and minified variations. You'll see something like this:
+
+```code
+my-project/
+├── docs/
+├── src/
+│   ├── favicon/
+│   ├── img/
+│   ├── js/
+│   ├── scss/
+│   ├── ...
+│   ├── index.html
+│   └── ...
+└── package.json
+```
--- src/js/script.js
+++ src/js/script.js
@@ -1,93 +1,101 @@
-$(window).load(() => {
-  // Page loader
-
-  $('body').imagesLoaded(() => {
-    $('.page-loader div').fadeOut();
-    $('.page-loader')
-      .delay(200)
-      .fadeOut('slow');
-  });
-
-  $(window).trigger('scroll');
-  $(window).trigger('resize');
-});
-
-$(document).ready(() => {
-  $(window).trigger('resize');
-});
-
-$(window).resize(() => {
-  js_height_init();
-});
-
-function js_height_init() {
-  (function($) {
-    $('.js-height-full').height($(window).height());
-    $('.js-height-parent').each(function() {
-      $(this).height(
-        $(this)
-          .parent()
-          .first()
-          .height()
-      );
-    });
-  })(jQuery);
+function jsHeightInit() {
+	$(".js-height-full").height($(window).height());
+	$(".js-height-parent").each(function() {
+		$(this).height(
+			$(this)
+				.parent()
+				.first()
+				.height()
+		);
+	});
 }
 
 function initMap() {
-  const bounds = new google.maps.LatLngBounds();
+	const bounds = new google.maps.LatLngBounds();
 
-  const locations = [
-    ['Colorado Convention Center', 39.743163, -104.9971202, 1],
-    ['Hyatt Regency Denver at Colorado Convention Center', 39.7433585, -104.995759, 2],
-  ];
+	const locations = [
+		["Colorado Convention Center", 39.743163, -104.9971202, 1],
+		[
+			"Hyatt Regency Denver at Colorado Convention Center",
+			39.7433585,
+			-104.995759,
+			2
+		]
+	];
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: new google.maps.LatLng(39.743163, -104.9971202),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  });
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 16,
+		center: new google.maps.LatLng(39.743163, -104.9971202),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
 
-  const infowindow = new google.maps.InfoWindow();
+	const infowindow = new google.maps.InfoWindow();
 
-  let marker, i;
+	let marker;
+	let i;
 
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map,
-    });
+	for (i = 0; i < locations.length; i += 1) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map
+		});
 
-    bounds.extend(marker.position);
+		bounds.extend(marker.position);
 
-    google.maps.event.addListener(
-      marker,
-      'click',
-      (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        };
-      })(marker, i)
-    );
-  }
-  map.fitBounds(bounds);
-}
-
-function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show');
+		google.maps.event.addListener(
+			marker,
+			"click",
+			(function(marker, i) {
+				return function() {
+					infowindow.setContent(locations[i][0]);
+					infowindow.open(map, marker);
+				};
+			})(marker, i)
+		);
+	}
+	map.fitBounds(bounds);
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+	if (!event.target.matches(".dropbtn")) {
+		const dropdowns = document.getElementsByClassName("dropdown-content");
+		let i;
+		for (i = 0; i < dropdowns.length; i += 1) {
+			const openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains("show")) {
+				openDropdown.classList.remove("show");
+			}
+		}
+	}
 };
+
+$(".dropbtn").click(() => {
+	$("#myDropdown").toggleClass("show");
+});
+
+$(window).load(() => {
+	// Page loader
+
+	$("body").imagesLoaded(() => {
+		$(".page-loader div").fadeOut();
+		$(".page-loader")
+			.delay(200)
+			.fadeOut("slow");
+	});
+
+	$(window).trigger("scroll");
+	$(window).trigger("resize");
+});
+
+$(document).ready(() => {
+	$(window).trigger("resize");
+
+	window.onload = function() {
+		initMap();
+	};
+});
+
+$(window).resize(() => {
+	jsHeightInit();
+});
